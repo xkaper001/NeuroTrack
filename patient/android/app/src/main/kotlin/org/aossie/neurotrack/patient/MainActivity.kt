68f8b7f83a55700f6349be0434cdf74dc5af41d1
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2025-03-12 19:46:33 +0530

    scaffold both the apps

---
+++ patient/android/app/src/main/kotlin/org/aossie/neurotrack/patient/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.aossie.neurotrack.patient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- therapist/android/app/src/main/kotlin/org/aossie/neurotrack/therapist/MainActivity.kt
+++ therapist/android/app/src/main/kotlin/org/aossie/neurotrack/therapist/MainActivity.kt
@@ -2,4 +2,4 @@ package org.aossie.neurotrack.therapist
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
