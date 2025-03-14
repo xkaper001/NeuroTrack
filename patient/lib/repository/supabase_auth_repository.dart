517492e348d3b54eeda847b58eaaf45d3209c8e4
Prakhar Singh <prakhar22153054@akgec.ac.in>
2025-03-15 00:23:49 +0530

    implement UserModel, AuthProvider, and SupabaseAuthRepository classes

---
+++ patient/lib/repository/supabase_auth_repository.dart
@@ -0,0 +1,8 @@
+import 'package:patient/core/repository/auth_repository.dart';
+
+class SupabaseAuthRepository implements AuthRepository {
+  // The repository class will implement the abstract repository class.
+  // Future<T> SignInWithGoogle() {
+  //  supabaase.auth.signInWithGoogle();
+  // }
+}
