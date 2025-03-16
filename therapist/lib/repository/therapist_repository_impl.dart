b2206097fd4a8fecd2348e78860e34d84a8b0c70
Mohd Jasir Khan <khanmohdjasir@gmail.com>
2025-03-16 15:49:31 +0530

    feat: try/catch in repo

--- therapist/lib/repository/therapist_repository_impl.dart
+++ therapist/lib/repository/therapist_repository_impl.dart
@@ -18,49 +18,61 @@ class TherapistRepositoryImpl implements TherapistRepository {
 
   @override
   Future<ActionResult> changeAppointmentStatus(String appointmentId, String status) async {
-    final response = await _supabaseClient.from('session')
+    try {
+      await _supabaseClient.from('session')
       .update({'status': status})
       .eq('id', appointmentId);
-    
-    if (response.error != null) {
-      return ActionResultFailure(errorMessage: response.error!.message, statusCode: response.error!.code);
+  
+     return ActionResultSuccess(data: 'Appointment Update Successfully', statusCode: 200);
+    } catch(e) {
+      return ActionResultFailure(errorMessage: e.toString(), statusCode: 400);
     }
-
-    return ActionResultSuccess(data: 'Appointment Update Successfully', statusCode: 200);
   }
 
   @override
   Future<ActionResult> getTherapistPatients() async {
-    final response = await _supabaseClient.from('patients')
+   try {
+      final response = await _supabaseClient.from('patients')
       .select('*')
       .eq('therapist_id', _supabaseClient.auth.currentUser!.id);
     
-    final data = response.map((data) => TherapistPatientDetailsEntityMapper.fromMap(data)).toList();
+      final data = response.map((data) => TherapistPatientDetailsEntityMapper.fromMap(data)).toList();
 
-    return ActionResultSuccess(data: data, statusCode: 200);   
+      return ActionResultSuccess(data: data, statusCode: 200); 
+    } catch(e) {
+      return ActionResultFailure(errorMessage: e.toString(), statusCode: 400);
+   } 
   }
 
   @override
   Future<ActionResult> getTherapistSchedule() async {
-    final response = await _supabaseClient.from('session')
+   try {
+      final response = await _supabaseClient.from('session')
       .select('*')
       .eq('therapist_id', _supabaseClient.auth.currentUser!.id);
     
-    final data = response.map((data) => TherapistScheduleEntityMapper.fromMap(data)).toList();
+      final data = response.map((data) => TherapistScheduleEntityMapper.fromMap(data)).toList();
 
-    return ActionResultSuccess(data: data, statusCode: 200); 
+      return ActionResultSuccess(data: data, statusCode: 200); 
+    } catch(e) {
+      return ActionResultFailure(errorMessage: e.toString(), statusCode: 400);
+   }
   }
 
   @override
   Future<ActionResult> getTherapistUpcomingAppointments() async {
-    final response = await _supabaseClient.from('session')
+    try {
+       final response = await _supabaseClient.from('session')
       .select('*')
       .eq('therapist_id', _supabaseClient.auth.currentUser!.id)
       .eq('status', 'Pending');
     
-    final data = response.map((data) => TherapistUpcomingAppointmentEntityMapper.fromMap(data)).toList();
+      final data = response.map((data) => TherapistUpcomingAppointmentEntityMapper.fromMap(data)).toList();
 
-    return ActionResultSuccess(data: data, statusCode: 200); 
+      return ActionResultSuccess(data: data, statusCode: 200); 
+    } catch(e) {
+      return ActionResultFailure(errorMessage: e.toString(), statusCode: 400);
+    }
   }
   
   @override
