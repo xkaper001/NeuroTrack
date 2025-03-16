a43421dea1cbc8de4015bd537f35e5f57bb954bc
Aqib Owais <owaisharoon60@gmail.com>
2025-03-16 17:08:34 +0500

    Adjust button size, increase question spacing, reduce option spacing in assessment screenand resolve conflicts

--- patient/lib/main.dart
+++ patient/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:patient/presentation/auth/auth_screen.dart';
+import 'package:patient/presentation/services/theme.dart';
+import 'package:patient/presentation/splash_screen.dart';
 import 'package:patient/provider/assessment_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -12,6 +14,15 @@ Future<void> main() async {
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -27,11 +38,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Patient App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: 'Poppins',
-        ),
-        home: const AuthScreen(),
+        theme: AppTheme.lightTheme(),
+        home: const SplashScreen(),
       ),
     );
   }
--- patient/lib/presentation/splash_screen.dart
+++ patient/lib/presentation/splash_screen.dart
@@ -15,12 +15,12 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const AuthScreen()),
         );
-      });
+      }
     });
   }
 
