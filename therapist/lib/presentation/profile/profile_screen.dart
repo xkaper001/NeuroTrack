3cdb1e416ea5ed8f1674a9e9ab461ff69bc383a5
abhinav7x94 <raj.abhinav7x94@gmail.com>
2025-03-22 14:42:22 +0530

    Final requested Changes

--- therapist/lib/presentation/profile/profile_screen.dart
+++ therapist/lib/presentation/profile/profile_screen.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widget/profile_option.dart'; // Fixed double slash
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProfileScreen(),
-    );
-  }
-}
+import '../widget/profile_option.dart'; 
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
